814af4f37b315836d8f83ff3a396745334786c41
Miguel Alves <162838147+maalvesdev@users.noreply.github.com>
2026-03-05 15:57:09 -0300

    Update aula1.py

--- Data Processing Fundamentals/Programming with Python/Python Introduction Exercises/Python - Aula 1/aula1.py
+++ Data Processing Fundamentals/Programming with Python/Python Introduction Exercises/Python - Aula 1/aula1.py
@@ -2,7 +2,6 @@
 tempo = 130
 print("Horas:", tempo//60, "Minutos:", tempo%60)
 
-
 #1. Calcule a média de três notas (7.5, 8.0, 6.5)
 nota1 = 7.5
 nota2 = 8.0
@@ -10,18 +9,15 @@ nota3 = 6.5
 media = (nota1+nota2+nota3) / 3
 print(media)
 
-
 #2. Mostrar a palavra 'programa' sem a primeira e a última letra
 texto = "programa"
 print(texto[1:-1])
-
 
 #3. Concatenar partes de uma string "abcdefghij"
 # primeiras letras
 # últimas letras
 texto = "abcdefghij"
 print(texto[:3], texto[-2:])
-
 
 #4. Mostrar o nome completo juntando nome e sobrenome
 #Nome: Ana
@@ -30,7 +26,6 @@ nome = "Ana"
 sobrenome = "Silva"
 print(nome,sobrenome)
 
-
 #5. Simule censura de parte da frase com asteriscos
 #frase = "Esse conteúdo é secreto"
 #Trocar "conteúdo" por "*" por exemplo
@@ -38,13 +33,11 @@ frase = "Esse conteúdo é secreto"
 censura = "*"
 print(frase[:5], censura, frase[-10:])
 
-
 #6. Crie um padrão alternando fatias diferentes
 a = "abcd"
 b = "1234"
 #Saída esperada: "a1b2c3d4"
 print(a[0]+b[0]+a[1]+b[1]+a[2]+b[2]+a[3]+b[3])
-
 
 #7. Verificar se a soma de dois números é par
 a = 8
@@ -52,13 +45,11 @@ b = 4
 soma = a + b
 print(soma%2)
 
-
 #8. calculo do IMC
 peso = float(input("Digite seu peso: "))
 altura = float(input("Digite sua altura: "))
 imc = peso / (altura*altura)
 print(imc)
-
 
 #9. Transforme de celsius para fahrenheit
 celsius = float(input("Digite a temperatura em Celsius: "))
